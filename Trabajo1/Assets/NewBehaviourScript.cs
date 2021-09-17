8541254ec8ec55c0d36f2903ee5079d8ac3e6daf
LauFranky <laufranky97@gmail.com>
2021-09-17 19:04:41 +0200

    Ejercicio 1.c
    Operaciones con variables de números y textos

--- Trabajo1/Assets/NewBehaviourScript.cs
+++ Trabajo1/Assets/NewBehaviourScript.cs
@@ -6,6 +6,8 @@ public class NewBehaviourScript : MonoBehaviour
 {
 
     public string myVar = "Hola jugador";
+    public string myVar2 = "Estas preparado para la aventura?";
+
     public int myInt = 123;
     public float myFloat = 2.5f;
     public bool myBool = true;
@@ -16,6 +18,8 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         print(myVar + myInt + myFloat + myBool);
+        print(myInt + myFloat);
+        print(myVar + myVar2);
     }
 
     // Update is called once per frame
