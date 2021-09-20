28c8c6c3bba88656cf91a92b03e81c67dab29f10
LauFranky <laufranky97@gmail.com>
2021-09-20 18:44:13 +0200

    Ejercicio 2.c
    Cuando el contador alcanza ciertas cifras, envía distintos mensajes.

--- Trabajo1/Assets/NewBehaviourScript.cs
+++ Trabajo1/Assets/NewBehaviourScript.cs
@@ -12,25 +12,31 @@ public class NewBehaviourScript : MonoBehaviour
     public float myFloat = 2.5f;
     public bool myBool = true;
 
-    public int Edad = 12;
-    
-    
+    public int Edad = 1;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-      while (Edad <= 20)
+        for(Edad = 1; Edad <20; Edad++)
         {
-            print(Edad);
-            Edad++;
+            if (Edad <= 11)
+            {
+                print("Soy un niño felis :D");
+            }
+            else if (Edad > 12 && Edad <= 18)
+            {
+                print("Tengo la edad del pavo");
+            }
+            else
+            {
+                print("Vaya por dios, soy adulto :(");
+            }
         }
-      for (myInt = 123; myInt <= 140;)
-        {
-            print(myInt);
-            myInt++;
-        }
-    }
 
+
+    }
     // Update is called once per frame
     void Update()
     {
