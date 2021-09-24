450601e7e94a3d388016207fa314289bd69fb78e
LauFranky <laufranky97@gmail.com>
2021-09-24 17:03:51 +0200

    Ejercicio 3.a
    Devuelve saludo con un nombre.

--- Trabajo1/Assets/NewBehaviourScript.cs
+++ Trabajo1/Assets/NewBehaviourScript.cs
@@ -5,31 +5,17 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
 
-    public string myVar = "Hola jugador";
-    public string myVar2 = "Estas preparado para la aventura?";
-
-    public int myInt = 123;
-    public float myFloat = 2.5f;
-    public bool myBool = true;
-
-    public int Edad = 1;
-
-    public int myNumber;
-
 
     // Start is called before the first frame update
     void Start()
     {
-        while(myNumber != 6)
-        {
-            myNumber = Random.Range(0, 20);
-            print("El número es " + myNumber);
-        }
-
+        Saludar("Franky");
+        Saludar("Yaiza");
     }
-    // Update is called once per frame
-    void Update()
+    
+    // Saludito
+    void Saludar(string hola)
     {
-        
+        print("Hola" + hola);
     }
 }
